9925b38246c4874676daab228ff13ea699e8d5bc
Alex Nepsha <alex.n@fundomate.com>
2023-11-05 15:38:50 +0300

    Test

--- index.ts
+++ index.ts
@@ -1,12 +1,7 @@
 import { app } from './src';
-import dotenv from 'dotenv';
-dotenv.config();
 
-import { init } from "./src/db";
 const port = process.env.PORT || 8000;
 
-init().then(() => {
-    app.listen(port, () => {
-        console.log(`Server running at http://localhost:${port}`);
-    });
-})
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,27 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import ideaRoutes from '../src/routes/ideas';
+import { init } from "./db";
 const app: Express = express();
 
-app.use(express.json()); // Add this line to enable JSON parsing in the request body
+init().then(() => {
+	app.use(express.json()); // Add this line to enable JSON parsing in the request body
 
-app.use('/api/ideas', ideaRoutes);
-app.use('/api/test', (req, res) => {
-	res.json({
-		test: 'test'
-	})
-});
+	app.use('/api/ideas', ideaRoutes);
+	app.use('/api/test', (req, res) => {
+		res.json({
+			test: 'test'
+		})
+	});
 
-app.get('/', (req: Request, res: Response) => {
-	res.send('Hello, TypeScript Express!');
-});
+	app.get('/', (req: Request, res: Response) => {
+		res.send('Hello, TypeScript Express!');
+	});
 
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-	console.error(err.stack);
-	res.status(500).send('Something went wrong');
-});
+	app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+		console.error(err.stack);
+		res.status(500).send('Something went wrong');
+	});
+})
 
 export { app };
