acada714f34d0e93ff696bcb76fce348aadd457e
Alex Nepsha <alex.n@fundomate.com>
2023-11-05 21:50:38 +0300

    Add api version

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,10 @@ dotenv.config();
 import ideaRoutes from '../src/routes/ideas';
 const app: Express = express();
 
-app.use(express.json()); // Add this line to enable JSON parsing in the request body
+// Enables JSON parsing in the request body
+app.use(express.json());
 
-app.use('/api/ideas', ideaRoutes);
-app.use('/api/test', (req, res) => {
-	res.json({
-		test: 'test-2',
-		user: process.env.DATABASE_USER
-	})
-});
+app.use('/api/v1/ideas', ideaRoutes);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 	console.error(err.stack);
