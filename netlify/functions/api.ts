497e8c147ba3f242be2fb0d2187b47d3fc5e54b7
Alex Nepsha <alex.n@fundomate.com>
2023-11-05 12:14:40 +0300

    test

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,4 +1,11 @@
-import { app } from "../../index";
+import express, { Router } from "express";
 import serverless from "serverless-http";
 
-export const handler = serverless(app);
+const api = express();
+
+const router = Router();
+router.get("/hello", (req, res) => res.send("Hello World!"));
+
+api.use("/api/", router);
+
+export const handler = serverless(api);
