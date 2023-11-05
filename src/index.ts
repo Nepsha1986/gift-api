bd05940b790b24b726cecad061441b80530e22dd
Alex Nepsha <alex.n@fundomate.com>
2023-11-05 13:49:36 +0300

    Test

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app: Express = express();
 
 app.use(express.json()); // Add this line to enable JSON parsing in the request body
 
-app.use('/ideas', ideaRoutes);
+app.use('/api/ideas', ideaRoutes);
 app.use('/api/test', (req, res) => {
 	res.json({
 		test: 'test'
