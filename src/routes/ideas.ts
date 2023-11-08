9fe486a4b156b138a14c64044df3f6f56868d8c3
Alex Nepsha <nepsha1986@gmail.com>
2023-11-08 15:35:57 +0300

    Admin draft (#5)
    Co-authored-by: Alex Nepsha <alex.n@fundomate.com>

--- src/routes/ideas.ts
+++ src/routes/ideas.ts
@@ -1,13 +1,13 @@
 import { Request, Response, Router } from 'express';
-import { getIdea, getIdeas } from "../controllers/ideaController";
+import { getIdea, getIdeas, getRelatedProducts, update } from "../controllers/ideaController";
 import { body, validationResult } from "express-validator";
 import { addIdea } from "../controllers/ideaController";
 
 const router = Router();
 
 const IdeaValidationRules = [
-	body('_ref_id').notEmpty().isString().withMessage('Reference id is required'),
-	body('name').notEmpty().isString().withMessage('Name is required'),
+	body('refId').notEmpty().isString().withMessage('Reference id is required'),
+	body('locale').notEmpty().withMessage('Locale is required').isLocale().withMessage('Should be a valid locale'),
 	body('products').isArray(),
 	body('products.*.title').notEmpty().isString(),
 	body('products.*.description').notEmpty().isString(),
@@ -15,7 +15,8 @@ const IdeaValidationRules = [
 ];
 
 router.get('/', getIdeas);
-router.get('/:_ref_id', getIdea);
+router.get('/:refId', getIdea);
+router.get('/:locale/:refId', getRelatedProducts);
 router.post('/', IdeaValidationRules, (req: Request, res: Response) => {
 	const errors = validationResult(req);
 
@@ -26,27 +27,8 @@ router.post('/', IdeaValidationRules, (req: Request, res: Response) => {
 	return addIdea(req, res);
 });
 
-// Examples:
-// router.put('/:id', taskValidationRules, (req: Request, res: Response) => {
-// 	const errors = validationResult(req);
-//
-// 	if (!errors.isEmpty()) {
-// 		return res.status(400).json({ errors: errors.array() });
-// 	}
-//
-// 	const product = products.find((t) => t.id === parseInt(req.params.id));
-//
-// 	if (!product) {
-// 		res.status(404).send('Product not found');
-// 	} else {
-// 		product.title = req.body.title || product.title;
-// 		product.description = req.body.description || product.description;
-// 		product.link = req.body.link || product.link;
-//
-// 		res.json(product);
-// 	}
-// });
-//
+router.put('/:id', update);
+
 // router.delete('/:id', (req: Request, res: Response) => {
 // 	const index = products.findIndex((t) => t.id === parseInt(req.params.id));
 //
