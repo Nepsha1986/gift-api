01bd3ca68bb7c02365cb2a038975e9de50e6afbe
Alex Nepsha <nepsha1986@gmail.com>
2023-11-09 17:19:31 +0300

    Split to prod/dev database

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # gift-api
+
+## Development
+
+### Database
+At this time we do not have a strict flow for development. That is why we are trying to keep DB dumps locally.
+As we are using MongoDB Atlass as a service, we are making dumps mannually and save them locally, we are using 'mongodump' and 'mongorestore' for this reason.
+
+The flow is to make a dump of production database:
+
+```
+    mongodump --uri=<mongodb-uri> -d=gift-ideas
+```
+After dump is ready, it will create a folder "gift-ideas". To create a dev version - rename "gift-ideas" to "gift-ideas_dev" and run:
+
+```
+ mongorestore --uri=<mongodb-uri>
+```
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,12 @@
 import { Db, MongoClient, ServerApiVersion } from 'mongodb';
 
+const env: 'development' | 'production' = process.env.NODE_ENV as any; // Can be 'development' or 'production';
+
+const database: Record<typeof env, string> = {
+	development: 'gift-ideas_dev',
+	production: 'gift-ideas',
+}
+
 const uri = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASS}@atlascluster.rkhjcgx.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -9,11 +16,10 @@ const client = new MongoClient(uri, {
 		deprecationErrors: true,
 	}
 });
-
 let db: Db;
 const init = () =>
 	client.connect().then(() => {
-		db = client.db('gift-ideas');
+		db = client.db(database[process.env.NODE_ENV as typeof env]);
 	})
 
 export { init, db };
