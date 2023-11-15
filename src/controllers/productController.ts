9dfd7be5fe3efad03d1f2bdd8a23c47cda4f5ff2
Alex Nepsha <nepsha1986@gmail.com>
2023-11-15 17:50:42 +0300

    Paginated products

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -6,10 +6,20 @@ import { Product } from "../models/Product";
 
 const DB_COLLECTION = 'products';
 
-export const add = async (req: Request, res: Response) => {
-	const { title, description, refId, locale, link } = req.body as Product;
+type PageableQuery = {
+	page: string,
+	pageSize: string;
+}
 
-	const data: Omit<Product , '_id'> = {
+type FilterQuery = {
+	refId: string,
+	locale: string,
+}
+
+export const add = async (req: Request, res: Response) => {
+	const {title, description, refId, locale, link} = req.body as Product;
+
+	const data: Omit<Product, '_id'> = {
 		title,
 		description,
 		refId,
@@ -22,26 +32,42 @@ export const add = async (req: Request, res: Response) => {
 }
 
 export const getAll = async (req: Request, res: Response) => {
-	const queryParams = req.query;
+	const {page, pageSize, refId, ...rest} = req.query as PageableQuery & FilterQuery;
+	const pageParsed: number = parseInt(page) || 1;
+	const pageSizeParsed: number = parseInt(pageSize) || 10;
 
-	const data = await db.collection(DB_COLLECTION).find(queryParams).toArray();
+	try {
+		const totalItems = await db.collection(DB_COLLECTION).countDocuments();
+		const totalPages = Math.ceil(totalItems / pageSizeParsed);
 
-	res.json(data);
+		const items = await db.collection(DB_COLLECTION).find({...rest}).skip((pageParsed - 1) * pageSizeParsed).limit(pageSizeParsed).toArray();
+
+		res.json({
+			items,
+			page: pageParsed,
+			pageSize: pageSizeParsed,
+			totalItems,
+			totalPages,
+		});
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({error: 'Internal Server Error'});
+	}
 };
 
 export const deleteItem = async (req: Request, res: Response) => {
 	const _id: ObjectId = new ObjectId(req.params.id);
 
 	try {
-		const data = await db.collection(DB_COLLECTION).deleteOne({ _id });
+		const data = await db.collection(DB_COLLECTION).deleteOne({_id});
 
 		if (data.deletedCount === 1) {
 			res.status(204).send();
 		} else {
-			res.status(404).json({ error: 'Product not found' });
+			res.status(404).json({error: 'Product not found'});
 		}
 	} catch (error) {
 		console.error('Error deleting product:', error);
-		res.status(500).json({ error: 'Internal server error' });
+		res.status(500).json({error: 'Internal server error'});
 	}
 }
